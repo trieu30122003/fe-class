e48eabda09538f3c536fca410f287e74f4b1b3c6
ĐỖ PHƯƠNG NAM <namdp1207@gmail.com>
2023-07-17 22:54:35 +0700

    nhanvien

--- src/router.js
+++ src/router.js
@@ -3,10 +3,13 @@ import Home from "./pages/admin/home/Home";
 import ListKhachHang from "./pages/KhachHang/ListKhachHang";
 import ListSanPham from "./pages/SanPham/ListSanPham";
 import AddSP from "./pages/SanPham/CreateSanPham";
+import ListNhanVien from "./pages/NhanVien/ListNhanVien";
+
 
 const publicRouters = [
     {path: '/', element: Home},
     {path: '/new/', element: ListKhachHang},
+    {path: '/nhan-vien/new/', element: ListNhanVien},
     {path: '/home', element: UserHome},
     {path: '/products/', element: ListSanPham},
     {path: '/addSP', element: AddSP}
