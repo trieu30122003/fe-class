715d03dfa78086434e43b8e8c5ddd4ee249f907d
ĐỖ PHƯƠNG NAM <namdp1207@gmail.com>
2023-07-21 15:04:30 +0700

    router

--- src/router.js
+++ src/router.js
@@ -3,10 +3,13 @@ import Home from "./pages/admin/home/Home";
 import ListKhachHang from "./pages/KhachHang/ListKhachHang";
 import CreateKhachHang from "./pages/KhachHang/CreateKhachHang";
 import ViewKhachHang from "./pages/KhachHang/ViewKhachHang";
-
+import ListNhanVien from "./pages/NhanVien/ListNhanVien";
+import CreateNhanVien from "./pages/NhanVien/CreateNhanVien";
 const publicRouters = [
     {path: '/', element: Home},
     {path: '/new/', element: ListKhachHang},
+    {path: '/nhan-vien/new/', element: ListNhanVien},
+    {path: '/nhan-vien/add/', element: CreateNhanVien},
     {path: '/home', element: UserHome},
     {path: '/new/add', element: CreateKhachHang},
     {path: '/new/:id', element: ViewKhachHang},
