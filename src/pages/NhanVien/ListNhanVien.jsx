dad4d2601c56ab97decf8336681f158f6200cfb3
ACER <tranducdo2811@gmail.com>
2023-07-21 14:53:18 +0700

    update hdct

--- src/pages/NhanVien/ListNhanVien.jsx
+++ src/pages/NhanVien/ListNhanVien.jsx
@@ -73,7 +73,6 @@ class ListNhanVien extends Component {
                                                     <td> {nv.gioiTinh}</td>
                                                     <td> {nv.diaChi}</td>
                                                     <td> {nv.sdt}</td>
-                                                    
                                                     <td> {nv.trangThai == 1 ? "Hoạt động" : "Ngừng hoạt động"}</td>
                                                     <td>
                                                         <button onClick={() => this.editEmployee(nv.id)} className="btn btn-info">Update </button>
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import ListSanPham from "./pages/SanPham/ListSanPham";
 import AddSP from "./pages/SanPham/CreateSanPham";
 import ListNhanVien from "./pages/NhanVien/ListNhanVien";
 import ListVoucher from "./pages/Voucher/ListVoucher";
+import ListHoaDon from "./pages/HoaDon/ListHoaDon";
 
 const publicRouters = [
     {path: '/', element: Home},
@@ -13,7 +14,8 @@ const publicRouters = [
     {path: '/home', element: UserHome},
     {path: '/voucher/new/', element: ListVoucher},
     {path: '/products/', element: ListSanPham},
-    {path: '/addSP', element: AddSP}
+    {path: '/addSP', element: AddSP},
+    {path: '/hdct/hien-thi/', element: ListHoaDon}
 ]
 const privateRouters = []
 
